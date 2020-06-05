2980ddfe96be1dcd12f4382b77c7e7794c2ef31e
Davide Cerato <mrc3drik@gmail.com>
2020-06-05 20:44:18 +0200

    mi card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.teal[800],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/me.jpg"),
+              ),
+              Text(
+                "Davide Cerato",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "DEVELOPMENT STUDENT",
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontSize: 18.0,
+                  letterSpacing: 2.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Source Sans Pro",
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 160.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+39 331 779 3649",
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontSize: 20.0,
+                      fontFamily: "Source Sans Pro",
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "CERATO.DAVIDE2002@GMAIL.COM",
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontSize: 19.0,
+                        fontFamily: "Source Sans Pro"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
